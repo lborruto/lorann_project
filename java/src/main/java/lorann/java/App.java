432b6ef1615b610fb1182579bb035293bf094021
Luca BORRUTO <luca.borruto@viacesi.fr>
2018-05-26 14:29:08 +0200

    pre-alpha

---
+++ java/src/main/java/lorann/java/App.java
@@ -0,0 +1,13 @@
+package lorann.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
