c92a0e9403bf84a3bc2f4f8af6b774383d2716d0
guillaumew74 <33539333+guillaumew74@users.noreply.github.com>
2018-05-28 16:11:00 +0200

    adding code

---
+++ java_project/controller/src/main/java/controller/Tick.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.lang.ModuleLayer.Controller;
+
+public class Tick {
+	
+	private long clock(){
+		return 0;
+		
+	}
+	
+	public void Tick(Controller controller){
+		
+	}
+	
+	public void run () {
+		
+	}
+	
+}
+
+
