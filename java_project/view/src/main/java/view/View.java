1a0d2e18072a62b49e0ff2dacc859a04e81575e3
Luca BORRUTO <luca.borruto@viacesi.fr>
2018-05-28 11:50:30 +0200

    adding code

---
+++ java_project/view/src/main/java/view/View.java
@@ -0,0 +1,54 @@
+package view;
+
+import java.awt.Frame;
+import java.awt.List;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import controller.IController;
+import model.IModel;
+
+public class View extends java.awt.JFrame {
+	
+	private String title;
+
+	private final Panel _panel;
+	private View() {
+		_panel = new Panel(null);
+	}
+	
+	public void View(IModel model, String title) {
+		
+	}
+	
+	public IModel getModel() {
+		return null;
+		
+	}
+	
+	public IController getController() {
+		return null;
+		
+	}
+	
+	public void setTitle(String title) {
+		
+	}
+	
+	public String getTitle() {
+		return title;
+		
+	}
+	
+	public void keyTyped(KeyEvent keyEvent) {
+		
+	}
+	
+	public void keyPressed(KeyEvent keyEvent) {
+		
+	}
+	
+	public void keyReleased(KeyEvent keyEvent) {
+		
+	}
+
+}
