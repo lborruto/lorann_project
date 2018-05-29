1dfb307ea470f936de83e90d778693ee5033517e
guillaumew74 <33539333+guillaumew74@users.noreply.github.com>
2018-05-29 14:45:09 +0200

    finition base de donnée

--- java_project/model/src/main/java/model/BDD.java
+++ java_project/model/src/main/java/model/BDD.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.beans.Statement;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,76 +12,155 @@ import java.util.ArrayList;
 
 public class BDD {
 
-	public static void main(String[] args) {
+	public BDD() {
+		int level;
+		for(level = 1; level <= 2; level++);                                        //
 		ArrayList<String> list = new ArrayList<String>();
+		
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/lorann?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 			java.sql.Statement stmt = cnx.createStatement();
 			
-			 InputStream is = new FileInputStream("");
-			 InputStreamReader lire = new InputStreamReader(is);
-			 BufferedReader buff = new BufferedReader(lire);
-			 
-			 
-			String ligne;
-			
-			while ((ligne = buff.readLine()) != null) {
-				list.add(ligne);
-			}
-			buff.close();
-			
-			int x, y;
-			for (y = 0; y <= 16; y++) {
-				String temp = list.get(y);
+			if(level == 1) {                                                     //level 1 
 				
-				for (x = 0; x <= 21; x++) {
+				InputStream is = new FileInputStream("");                     // mettre le chemin dacces des fichier texte du niveau 1
+																			  //mettre un nom relatif au lieu dun chemin absolu, 
+				InputStreamReader lire = new InputStreamReader(is);
+				BufferedReader buff = new BufferedReader(lire);
+			 
+			 
+				String ligne;
+			
+				while ((ligne = buff.readLine()) != null) {
+					list.add(ligne);
+				}
+				
+				buff.close();
+
+				stmt.executeUpdate("DROP TABLE lorann.level");
+				stmt.executeUpdate("CREATE TABLE level(ID int not null auto_increment, Structure varchar (150) NOT NULL, X int(20), Y int(20) NOT NULL, NumNiveau int(5) NOT NULL");
+				
+			}
+			
+			else if(level == 2) {
+				
+				InputStream is = new FileInputStream("");                     // mettre le chemin dacces des fichier texte du niveau 2
+				InputStreamReader lire = new InputStreamReader(is);
+				BufferedReader buff = new BufferedReader(lire);
+			 
+			 
+				String ligne;
+			
+				while ((ligne = buff.readLine()) != null) {
+					list.add(ligne);
+				}
+				
+				buff.close();
+				
+			}
+			
+			else if(level == 3) {
+				
+				InputStream is = new FileInputStream("");                     // mettre le chemin dacces des fichier texte du niveau 3
+				InputStreamReader lire = new InputStreamReader(is);
+				BufferedReader buff = new BufferedReader(lire);
+			 
+			 
+				String ligne;
+			
+				while ((ligne = buff.readLine()) != null) {
+					list.add(ligne);
+				}
+				
+				buff.close();
+				
+			}
+			
+			else if(level == 4) {
+				
+				InputStream is = new FileInputStream("");                     // mettre le chemin dacces des fichier texte du niveau 4
+				InputStreamReader lire = new InputStreamReader(is);
+				BufferedReader buff = new BufferedReader(lire);
+			 
+			 
+				String ligne;
+			
+				while ((ligne = buff.readLine()) != null) {
+					list.add(ligne);
+				}
+				
+				buff.close();
+				
+			}
+			
+			else if(level == 5) {
+				
+				InputStream is = new FileInputStream("");                     // mettre le chemin dacces des fichier texte du niveau 5
+				InputStreamReader lire = new InputStreamReader(is);
+				BufferedReader buff = new BufferedReader(lire);
+			 
+			 
+				String ligne;
+			
+				while ((ligne = buff.readLine()) != null) {
+					list.add(ligne);
+				}
+				
+				buff.close();
+				
+			}
+			
+				
+				int x, y;
+				for (y = 0; y <= 16; y++) {
+					String temp = list.get(y);
+				
+					for (x = 0; x <= 21; x++) {
 					
-					char sprite = temp.charAt(x);
+						char sprite = temp.charAt(x);
 					
-					switch(sprite) {
-					case '|':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"|\"," + x + "," + y + ", 1)");
-						break;
-					case '-':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"-\"," + x + "," + y + ", 1)");
-						break;
-					case 'O':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"O\"," + x + "," + y + ", 1)");
-						break;
-					case '.':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\".\"," + x + "," + y + ", 1)");
-						break;
-					case 'H':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"H\"," + x + "," + y + ", 1)");
-						break;
-					case 'i':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"i\"," + x + "," + y + ", 1)");
-						break;
-					case 'p':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"p\"," + x + "," + y + ", 1)");
-						break;
-					case 'x':
-						stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"x\"," + x + "," + y + ", 1)");
-						break;
-					default:
-						break;
+						switch(sprite) {
+						case '|':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"|\"," + x + "," + y + ", 1)");
+							break;
+						case '-':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"-\"," + x + "," + y + ", 1)");
+							break;
+						case 'O':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"O\"," + x + "," + y + ", 1)");
+							break;
+						case '.':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\".\"," + x + "," + y + ", 1)");
+							break;
+						case 'H':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"H\"," + x + "," + y + ", 1)");
+							break;
+						case 'i':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"i\"," + x + "," + y + ", 1)");
+							break;
+						case 'p':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"p\"," + x + "," + y + ", 1)");
+							break;
+						case 'x':
+							stmt.executeUpdate("INSERT INTO level(Structure, X, Y, NumNiveau) VALUES(\"x\"," + x + "," + y + ", 1)");
+							break;
+						default:
+							break;
+							
+						}
 						
 					}
-					
+						
+						
 				}
-					
-					
-			}
-			
-			stmt.close();
-			cnx.close();
-			} catch (ClassNotFoundException | SQLException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-
+				
+				stmt.close();
+				cnx.close();
+				} catch (ClassNotFoundException | SQLException | IOException e) {
+					e.printStackTrace();
+				}
+		}
 	
-}
-
+		
+	}
