41ede91d379567110123b9256e19c0acf618f227
Luca BORRUTO <luca.borruto@viacesi.fr>
2018-05-29 11:45:58 +0200

    pre-alpha

--- lorann_debug/src/lorann_debug/Model.java
+++ lorann_debug/src/lorann_debug/Model.java
@@ -1,7 +1,56 @@
 package lorann_debug;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Model {
 
 	Maps lvl = new Maps("C:/Users/lucab/OneDrive/Documents/GitHub/lorann_project/levels/lvl1.txt");
+	BufferedImage bf;
+
+	public Image getImage(char a) {
+		if (a == '-') {
+			try {
+				bf = ImageIO.read(new File("C:/Users/lucab/OneDrive/Documents/GitHub/lorann_project/sprites/horizontal_bone.png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			if (a == '|') {
+				try {
+					bf = ImageIO
+							.read(new File("C:/Users/lucab/OneDrive/Documents/GitHub/lorann_project/sprites/vertical_bone.png"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				if (a == '.') {
+					try {
+						bf = ImageIO.read(
+								new File("C:/Users/lucab/OneDrive/Documents/GitHub/lorann_project/sprites/bone.png"));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					if (a == 'H') {
+						try {
+							bf = ImageIO.read(new File(
+									"C:/Users/lucab/OneDrive/Documents/GitHub/lorann_project/sprites/lorann_u.png"));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					} else {
+						bf=null;
+						
+					}
+				}
+			}
+		}
+		return bf;
+	}
 
 }
